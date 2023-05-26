6b3bb67cde2995647eaf2aa3755b3ff8b2a4f59a
R2 <R2@MILA-SHKA>
2023-05-26 11:22:13 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Calculations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+   public class Calculations
+   {
+            public string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime,
+                TimeSpan endWorkingTime, int consultationTime)
+            {
+                List<string> result = new List<string>();
+                if (endWorkingTime <= beginWorkingTime)
+                    endWorkingTime += TimeSpan.FromHours(24);
+
+                var consultationSlotDuration = TimeSpan.FromMinutes(consultationTime);
+                var workStart = beginWorkingTime;
+                var breaks = startTimes
+                .Select(startTime => startTime <= beginWorkingTime ? startTime + TimeSpan.FromHours(24) : startTime)
+                .Zip(durations, (start, duration) => (start, end: start + TimeSpan.FromMinutes(duration)))
+                .Append((endWorkingTime, TimeSpan.Zero));
+
+                foreach (var (breakStart, breakEnd) in breaks)
+                {
+                    var slotCount = (int)((breakStart - workStart) / consultationSlotDuration);
+                    for (int j = 0; j < slotCount; j++)
+                    {
+                        var slotStart = workStart + consultationSlotDuration * j;
+                        var slotEnd = workStart + consultationSlotDuration * (j + 1);
+                        result.Add($"{slotStart:hh':'mm}-{slotEnd:hh':'mm}");
+                    }
+                    workStart = breakEnd;
+                }
+                return result.ToArray();
+            }
+        
+   }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp16;
+// See https://aka.ms/new-console-template for more information
+
+TimeSpan[] startTimes = new TimeSpan[]
+{
+ new TimeSpan (10,0,0),
+ new TimeSpan (11,0,0),
+ new TimeSpan (15,0,0),
+ new TimeSpan (15,30,0),
+ new TimeSpan (16,50,0),
+};
+int[] durations = new int[] { 60, 30, 10, 10, 40 };
+TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+TimeSpan endWorkingTime = new TimeSpan(18, 0, 0);
+int cosultaionTime = 30;
+
+Calculations calculations = new Calculations();
+string[] availableperiods = calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, cosultaionTime);
+foreach (var item in availableperiods)
+{
+    Console.WriteLine(item);
+}
